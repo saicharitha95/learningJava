9cdcc5f533de185893a417ddfa0c6b987e01918e
saicharitha95 <116802364+saicharitha95@users.noreply.github.com>
2022-10-27 04:10:51 -0700

    added Arthemetic Operations

---
+++ src/main/java/org/example/ArthemeticOperations.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class ArthemeticOperations {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+
+        int sum=a+b;
+        System.out.println("Addition of a+b= "+sum);
+
+
+        int minus=a-b;
+        System.out.println("Addition of a-b= "+minus);
+
+
+        int div=a%b;
+        System.out.println("Addition of a%b= "+div);
+
+
+        int mult=a*b;
+        System.out.println("Addition of a+b= "+mult);
+
+    }
+}
